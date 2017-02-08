9222bdf27321c571473307293a163b735be9e1dd
David Weinstein <txea@Davids-MacBook-Air.local>
2017-02-07 22:46:25 -0500

    done

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,23 @@
 export let state;
 
-
-export function managePets(){
+export function managePets(state= {pets:[]}, action){
+  switch (action.type) {
+    case 'ADD_PET':
+      return { ...state, pets: [...state.pets, action.payload] }
+    case 'REMOVE_PET':
+      let updatePets = state.pets.filter((pet) => pet.id !== action.payload)
+          return { ...state, pets: updatePets }
+    default:
+      return state
+  }
 }
 
-export function dispatch(){
+export function dispatch(action){
+  state = managePets(state, action)
+  render()
 }
 
 export function render(){
-
+  var petArray = state.pets.map( pet => `<li>${pet.name}</li>`)
+  document.getElementById('container').innerHtml = `<ul>${petArray.join()}</ul>`
 }
